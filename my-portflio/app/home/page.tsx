6940bb7f633d12bdd9561157a8ff9352b160a5a0
Delong Yang <delongyang369@gmail.com>
2024-10-01 23:07:26 +0100

    Login Page
    Added Login page

---
+++ my-portflio/app/home/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="h-[100vh] flex flex-col items-center justify-center">
+      <h1 className="text-center text-5xl font-bold text-red-400 mb-8">
+        Home Page
+      </h1>
+    </div>
+  );
+}
+
+export default Home;
--- my-portflio/app/page.tsx
+++ my-portflio/app/page.tsx
@@ -1,9 +1,47 @@
-import Image from "next/image";
+"use client"; // Add this directive at the top
 
-export default function Home() {
+import { useState } from 'react';
+import { useRouter } from 'next/navigation'; // Import useRouter
+import PinInput from 'react-pin-input'; // Import PinInput
+
+export default function LoginPage() {
+  const [error, setError] = useState('');
+  const router = useRouter(); // Initialize useRouter
+
+  const handleComplete = (value: string) => {
+    const correctPin = '1234'; // Replace with your actual PIN checking logic
+
+    if (value === correctPin) {
+      router.push('/home'); // Navigate to the home page
+    } else {
+      setError('Incorrect PIN. Please try again.');
+    }
+  };
+
   return (
-    <div>
-      <h1 className="text-center">Hello World</h1>
+    <div className="h-[100vh] flex flex-col items-center justify-center">
+      <h1 className="text-center text-5xl font-bold text-red-400 mb-8">
+        Login
+      </h1>
+      <div className="flex flex-col items-center">
+        <PinInput
+          length={4}
+          type="numeric"
+          inputMode="number"
+          style={{ padding: '10px' }}
+          inputStyle={{
+            width: '50px',
+            height: '50px',
+            fontSize: '24px',
+            textAlign: 'center',
+            borderColor: 'gray',
+            backgroundColor: '#ffffff',
+            color: '#171717',
+          }}
+          onComplete={handleComplete}
+        />
+        {error && <p className="text-red-500 mt-3">{error}</p>}
+      </div>
     </div>
   );
 }
