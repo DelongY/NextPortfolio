d49c2b7dd12924402f643c0b8f571f409c0054b9
Delong Yang <delongyang369@gmail.com>
2024-10-03 02:07:40 +0100

    Colour Theme

--- my-portflio/app/components/NavBar.tsx
+++ my-portflio/app/components/NavBar.tsx
@@ -1,23 +1,24 @@
 import React from 'react';
+import "../globals.css";
 
 function NavBar() {
   return (
-    <nav className="fixed top-0 left-0 w-full bg-blue-500 p-4 z-50 flex items-center">
-      <div className="flex-shrink-0 text-white text-xl font-bold mr-4">
+    <nav className="fixed top-0 left-0 w-full bg-accent p-4 z-50 flex items-center" style={{ backgroundColor: 'var(--color-background)' }}>
+      <div className="flex-shrink-0 text-highlight text-xl font-bold mr-4" style={{ color: 'var(--color-highlight)' }}>
         <a href="/">🍊Delong</a>
       </div>
       <ul className="flex justify-around flex-grow">
-        <li className="text-white">
+        <li className="text-highlight" style={{ color: 'var(--color-highlight)' }}>
           <a href="/pages/home">Home</a>
         </li>
-        <li className="text-white">
+        <li className="text-highlight" style={{ color: 'var(--color-highlight)' }}>
           <a href="/pages/portfolio">Portfolio</a>
         </li>
-        <li className="text-white">
+        <li className="text-highlight" style={{ color: 'var(--color-highlight)' }}>
           <a href="/pages/about">About me</a>
         </li>
-        <li className="text-white">
-          <a href="/pages//contact">Contact</a>
+        <li className="text-highlight" style={{ color: 'var(--color-highlight)' }}>
+          <a href="/pages/contact">Contact</a>
         </li>
       </ul>
     </nav>
--- my-portflio/app/pages/home/page.tsx
+++ my-portflio/app/pages/home/page.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import profile from './profile.jpg';
+
 const Home = () => {
   return (
     <div className="h-[100vh] flex flex-col items-center justify-center">
       <h1 className="text-center text-5xl font-bold text-red-500 mb-8">
         Home
       </h1>
+      <img src={profile}/>
     </div>
   );
 }
