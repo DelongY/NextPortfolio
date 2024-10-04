7a477bbdc2ce8be6602ae486402bad1aeb237c81
Delong Yang <delongyang369@gmail.com>
2024-10-05 00:54:19 +0100

    Update NavBar.tsx

--- my-portflio/app/components/NavBar.tsx
+++ my-portflio/app/components/NavBar.tsx
@@ -3,21 +3,21 @@ import "../globals.css";
 
 function NavBar() {
   return (
-    <nav className="fixed top-0 left-0 w-full bg-accent p-4 z-50 flex items-center" style={{ backgroundColor: 'var(--color-background)' }}>
-      <div className="flex-shrink-0 text-highlight text-xl font-bold mr-4" style={{ color: 'var(--color-highlight)' }}>
+    <nav className="fixed top-0 left-0 w-full bg-accent p-4 z-50 flex items-center">
+      <div className="flex-shrink-0 text-highlight text-xl font-bold mr-4">
         <a href="/">🍊Delong</a>
       </div>
       <ul className="flex justify-around flex-grow">
-        <li className="text-highlight" style={{ color: 'var(--color-highlight)' }}>
+        <li className="text-highlight">
           <a href="/pages/home">Home</a>
         </li>
-        <li className="text-highlight" style={{ color: 'var(--color-highlight)' }}>
+        <li className="text-highlight">
           <a href="/pages/portfolio">Portfolio</a>
         </li>
-        <li className="text-highlight" style={{ color: 'var(--color-highlight)' }}>
+        <li className="text-highlight">
           <a href="/pages/about">About me</a>
         </li>
-        <li className="text-highlight" style={{ color: 'var(--color-highlight)' }}>
+        <li className="text-highlight">
           <a href="/pages/contact">Contact</a>
         </li>
       </ul>
